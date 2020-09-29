a1b7ebedbe56b79d036f11c0054f10d03c7bd310
yuzheng <yuzheng@uniontech.com>
2020-09-29 09:59:59 +0800

    add oem-file edit date

--- add-kms.sh
+++ add-kms.sh
@@ -13,14 +13,14 @@ chose_arch
 chose_oem_file
 
 case $OEM_FILE in 
-	"01_installer.tgz") tar_oem_file
+	01_installer*.tgz) tar_oem_file
 	   change_kms "Vlc1cGIyNTBaV05v" "10.10.58.44" "8600"
 	   ;;
-	"02_checkmode-auto.tgz"|"03_checkmode-notauto.tgz") tar_oem_file
+	02_checkmode-auto*.tgz|03_checkmode-notauto*.tgz) tar_oem_file
 	   open_check_mode
            change_kms "Vlc1cGIyNTBaV05v" "10.10.58.44" "8600" 
 	   ;;
-	"thtf.tgz"|"lenovo.tgz"|"100trust-kms.tgz") tar_oem_file
+	thtf*.tgz|lenovo*.tgz|100trust-kms*.tgz) tar_oem_file
 	   open_check_mode
 	   ;;
 	*) echo -e "\033[31mInput is ERROR!!! \033[0m"
